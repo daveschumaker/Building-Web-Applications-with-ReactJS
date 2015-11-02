b8774e70150dd08d1da2188cf598e612e594b702
Dave Schumaker <daveschumaker@gmail.com>
2015-11-01 19:55:07 -0800

    Initial commit for my to-do list example.

---
+++ Lesson-7-Firebase/src/app.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var Header = require('./header');
+var rootUrl = require('./config');
+
+
+var App = React.createClass({
+  mixins: [ReactFire],
+  componentWillMount: function() {
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+    // this.state.items => {}
+  },
+
+  render: function() {
+    console.log(this.state);
+
+    return <div className="row panel panel-default">
+      <div className="col-md-8 col-md-offset-2">
+      <h2 className="text-center">
+        To-Do List
+      </h2>
+      <Header />
+      </div>
+    </div>
+  }
+});
+
+var element = React.createElement(App, {});
+React.render(element, document.querySelector('.container'));
